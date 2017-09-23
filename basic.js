bf85f075e5c058d2495d2eded43c0338de8c918f
Chris Fuess <cfuess@LIVE.COM>
2017-09-23 01:42:05 -0700

    Added comments to basic.js

--- basic.js
+++ basic.js
@@ -1,3 +1,4 @@
+// Chrome Dev Tools Console Games with JavaScript
 var a = 1;
 var b = 2;
 
@@ -5,3 +6,5 @@ var c = function () { return a + b; };
 var d = function () { return a * b; };
 
 var e = function () { console.log("a: " + a);console.log("b: " + b);console.log("c: " + c());console.log("d: " + d()); };
+
+// c, d, and e are functions call them like c(); d(); etc.
